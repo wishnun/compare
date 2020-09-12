d18a8305412cd13c6297dd9ee9a529065047cc92
johndoe434 <68118162+johndoe434@users.noreply.github.com>
2020-09-12 17:22:25 -0400

    mac

--- ncoa2.py
+++ ncoa2.py
@@ -1,6 +1,6 @@
 print ("comparing files...")
-with open('C:\\data\\ENV3\\NCOA\\new\\NcoaFileExtractboth20200912.txt', 'r') as file1:
-    with open('C:\\data\\ENV3\\NCOA\\old\\NcoaFileExtractboth20200911.txt', 'r') as file2:
+with open('1.txt', 'r') as file1:
+    with open('2.txt', 'r') as file2:
         difference_new = set(file1).difference(file2)
         difference_old = set(file2).difference(file1)
 
@@ -8,10 +8,10 @@ with open('C:\\data\\ENV3\\NCOA\\new\\NcoaFileExtractboth20200912.txt', 'r') as 
 difference_new.discard('\n')
 difference_old.discard('\n')
 
-with open('C:\\data\\ENV3\\NCOA\\ncoa_diff_out_new1.txt', 'w') as file_out1:
+with open('n1.txt', 'w') as file_out1:
     for line in difference_new:
         file_out1.write(line)
 
-with open('C:\\data\\ENV3\\NCOA\\ncoa_diff_out_old1.txt', 'w') as file_out2:
+with open('n2.txt', 'w') as file_out2:
     for line in difference_old:
         file_out2.write(line)
